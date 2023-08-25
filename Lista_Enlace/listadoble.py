fd60ab9205926c72b47d55324053f377a0e54421
Elias-C0 <141202551+Elias-C0@users.noreply.github.com>
2023-08-25 18:04:51 -0300

    funcion copiar

--- Lista_Enlace/listadoble.py
+++ Lista_Enlace/listadoble.py
@@ -7,49 +7,55 @@ class Nodo:
 
 class Lista_doble_enlace:
     def __init__(self):
-        self.primero = None
-        self.utimo = None
+        self.cabeza = None
+        self.cola = None
         self.tamanio= 0
 
     def esta_vacia(self):
-        return self.primero == None
+        return self.cabeza == None
 
     def agregar_al_final(self,dato):
         if self.esta_vacia():
-            self.primero = self.ultimo = Nodo(dato)
+            self.cabeza = self.cola = Nodo(dato)
         else:
-            aux=self.ultimo
-            self.ultimo = aux.siguiente = Nodo(dato)
-            self.ultimo.anterior= aux
+            aux=self.cola
+            self.cola = aux.siguiente = Nodo(dato)
+            self.cola.anterior= aux
         self.tamanio += 1
-
 
     def agregar_al_inicio(self,dato):
         if self.esta_vacia():
-            self.primero = self.ultimo = Nodo(dato)
+            self.cabeza = self.cola = Nodo(dato)
         else:
             aux = Nodo(dato)
-            aux.siguiente = self.primero
-            self.primero.anterior = None
-            self.primero = aux
+            aux.siguiente = self.cabeza
+            self.cabeza.anterior = None
+            self.cabeza = aux
         self.tamanio += 1
 
     def iterar_inicio(self):
-        aux= self.primero
+        aux= self.cabeza
         while aux != None:
             print(aux.dato)
             aux= aux.siguiente
     
     def iterar_final(self):
-        aux= self.ultimo
+        aux= self.cola
         while aux:
             print(aux.dato)
             aux = aux.anterior
 
     def tamanio(self):
         return self.tamanio
-        
-
+    
+    def copiar_lista(self):
+        copia_lista = Lista_doble_enlace()
+        actual = self.cabeza
+        while actual:
+            copia_lista.agregar(actual.dato)
+            actual = actual.siguiente
+        return copia_lista
+    
 aux= Lista_doble_enlace()
 a= aux.esta_vacia()
 print(a)
