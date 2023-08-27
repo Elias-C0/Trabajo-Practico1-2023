76e712e96b297c4c9b4155f0c8d93da93e8f87da
Elias-C0 <elias_prueba@hotmail.com>
2023-08-27 16:46:06 -0300

    correcion de tamanio a __len__ y iterar_al_inicio a __iter__

---
+++ TP1_LDE_testing/main.py
@@ -0,0 +1,69 @@
+class Nodo:
+    def __init__(self,dato):
+        self.dato = dato
+        self.siguiente = None
+        self.anterior= None
+
+
+class ListaDobleEnlazada:
+    def __init__(self):
+        self.cabeza = None
+        self.cola = None
+        self.tamano= 0
+
+    def esta_vacia(self):
+        return self.cabeza == None
+
+    def agregar_al_inicio(self,dato):
+        if self.esta_vacia():
+            self.cabeza = self.cola = Nodo(dato)
+        else:
+            aux = Nodo(dato)
+            aux.siguiente = self.cabeza
+            self.cabeza.anterior = aux
+            self.cabeza = aux
+        self.tamano += 1
+
+    def agregar_al_final(self,dato):
+        if self.esta_vacia():
+            self.cabeza = self.cola = Nodo(dato)
+        else:
+            aux= self.cola
+            self.cola = aux.siguiente = Nodo(dato)
+            self.cola.anterior= aux
+        self.tamano += 1
+
+    #REVISAR
+    def insertar(self,dato,posicion=None):
+        if self.esta_vacia():
+            self.cabeza = self.cola = Nodo(dato)
+        elif posicion < 0 or posicion > self.tamano:
+            raise ValueError("FUERA DE RANGO")
+        elif posicion == self.tamano:
+            self.agregar_al_final(dato)
+        self.tamano +=1
+
+    def __iter__(self):
+        aux = self.cabeza
+        while aux:
+            yield(aux.dato)
+            aux = aux.siguiente
+    
+    def iterar_final(self):
+        aux= self.cola
+        while aux:
+            print(aux.dato)
+            aux = aux.anterior
+
+    def __len__(self):
+        return self.tamano
+    
+    #Funciona, pero si queres imprimirlo no se imprime correctamente, no se si deberia ser asi.
+    def copiar(self):
+        copia_lista = ListaDobleEnlazada()
+        actual = self.cabeza
+        while actual != None:
+            copia_lista.agregar_al_final(actual.dato)
+            actual = actual.siguiente
+        return copia_lista
+    
