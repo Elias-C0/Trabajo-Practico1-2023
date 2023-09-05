25a862dd2d55f90544f887907f02e35f6d2fca42
Ayrton Milessi <ayrton4210@gmail.com>
2023-09-05 00:47:13 -0300

    cambio en el main para agregar mas nodos

--- Lista_Enlace/main.py
+++ Lista_Enlace/main.py
@@ -2,7 +2,17 @@ from listadoble import ListaDobleEnlazada
 
 aux= ListaDobleEnlazada()
 
-aux.agregar_al_inicio(30)
+aux.agregar_al_inicio(32)
+aux.agregar_al_final(71)
+aux.agregar_al_final(2)
+aux.agregar_al_final(29)
+aux.agregar_al_final(47)
+aux.agregar_al_final(-139)
+aux.agregar_al_final(-75)
+aux.agregar_al_final(121)
+aux.agregar_al_final(25)
 
-a=aux.copiar()
+aux.ordenar()
 
+for x in aux:
+    print(x)
