e7b4f87adaa899bf45c59c86d9c8e6f923fd5485
Ayrton Milessi <ayrton4210@gmail.com>
2023-09-06 00:51:43 -0300

    grafico terminado

--- Lista_Enlace/main.py
+++ Lista_Enlace/main.py
@@ -2,18 +2,4 @@ from listadoble import ListaDobleEnlazada
 
 aux= ListaDobleEnlazada()
 
-aux.agregar_al_inicio(32)
-aux.agregar_al_final(71)
-aux.agregar_al_final(2)
-aux.agregar_al_final(29)
-aux.agregar_al_final(47)
-aux.agregar_al_final(-139)
-aux.agregar_al_final(-75)
-aux.agregar_al_final(121)
-aux.agregar_al_final(25)
-aux.agregar_al_inicio(55)
-
-aux.ordenar()
-
-for x in aux:
-    print(x)
+aux.grafico(10)
