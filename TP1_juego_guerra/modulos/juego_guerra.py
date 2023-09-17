a49207e7c2572f31cea4295c2604adf53358d021
Ayrton Milessi <ayrton4210@gmail.com>
2023-09-17 18:11:12 -0300

    guerra terminado

--- TP1_juego_guerra/modulos/juego_guerra.py
+++ TP1_juego_guerra/modulos/juego_guerra.py
@@ -14,7 +14,7 @@ class Carta:
   
 class Mazo:
   def __init__(self):
-    self.mazo= ListaDobleEnlazada()
+    self.mazo= ListaDobleEnlazada() #se usa la lista doblemente enlazada para el mazo
 
   def generar_mazo(self):
     for palo in palos: #Se le agregan las cartas con los valores y palos al mazo
@@ -53,7 +53,7 @@ class Mazo:
         self.ganador = otro_jugador
         print(f"El jugador {jugador} se quedó sin cartas para continuar con el juego")
         print(f"\n                               ***** Jugador {otro_jugador} gana la partida *****")
-        exit(1)
+        exit(1) #si un jugador no tiene cartas para jugar, se termina el juego
   
   def __iter__(self):
     return iter(self.mazo)
@@ -64,23 +64,77 @@ class JuegoDeGuerra:
     self.max_turnos = limite_turnos
     self.ganador = None
 
-  def imprimir_mazo(self, jugador):
+  def imprimir_mazo(self, jugador): #imprime el mazo de cada jugador
     for x in range(len(jugador.mazo)):
-      print("-X ", end=" ")
+      print("-X ", end=" ") 
       if (x + 1) % 10 == 0:
         print()
     print()
 
+  def imprimir_botin_guerra(self,botin): #imprime el botin en juego segun cuantas guerras seguidas se produjeron
+    lista_botin= list(botin)
+    if len(lista_botin) == 10:
+      print()
+      print(f"         {lista_botin[0]} {lista_botin[1]}", end=" ")
+      print("-X " * 6, end="")
+      print(f"{lista_botin[8]} {lista_botin[9]}")
+      print("\n")
+    elif len(lista_botin) == 18:
+      print()
+      print(f"         {lista_botin[0]} {lista_botin[1]}", end=" ")
+      print("-X " * 6, end="")
+      print(f"{lista_botin[8]} {lista_botin[9]}")
+      print("-X " * 6, end="")
+      print(f"{lista_botin[16]} {lista_botin[17]}")
+      print("\n")
+    elif len(lista_botin) == 26:
+      print()
+      print(f"         {lista_botin[0]} {lista_botin[1]}", end=" ")
+      print("-X " * 6, end="")
+      print(f"{lista_botin[8]} {lista_botin[9]}")
+      print("-X " * 6, end="")
+      print(f"{lista_botin[16]} {lista_botin[17]}")
+      print("-X " * 6, end="")
+      print(f"{lista_botin[24]} {lista_botin[25]}")
+      print("\n")
+    elif len(lista_botin) == 34:
+      print()
+      print(f"         {lista_botin[0]} {lista_botin[1]}", end=" ")
+      print("-X " * 6, end="")
+      print(f"{lista_botin[8]} {lista_botin[9]}")
+      print("-X " * 6, end="")
+      print(f"{lista_botin[16]} {lista_botin[17]}")
+      print("-X " * 6, end="")
+      print(f"{lista_botin[24]} {lista_botin[25]}")
+      print("-X " * 6, end="")
+      print(f"{lista_botin[32]} {lista_botin[33]}")
+      print("\n")
+    elif len(lista_botin) == 42:
+      print()
+      print(f"         {lista_botin[0]} {lista_botin[1]}", end=" ")
+      print("-X " * 6, end="")
+      print(f"{lista_botin[8]} {lista_botin[9]}")
+      print("-X " * 6, end="")
+      print(f"{lista_botin[16]} {lista_botin[17]}")
+      print("-X " * 6, end="")
+      print(f"{lista_botin[24]} {lista_botin[25]}")
+      print("-X " * 6, end="")
+      print(f"{lista_botin[32]} {lista_botin[33]}")
+      print("-X " * 6, end="")
+      print(f"{lista_botin[40]} {lista_botin[41]}")
+      print("\n")
+
   def jugar_ronda(self):
-    carta_jugador_1 = self.jugador_1.sacar_arriba(1)
+    carta_jugador_1 = self.jugador_1.sacar_arriba(1) #se extraen las cartas de arriba del mazo de cada jugador
     carta_jugador_2 = self.jugador_2.sacar_arriba(2)
 
-    if carta_jugador_1 is None or carta_jugador_2 is None:
+    if carta_jugador_1 is None or carta_jugador_2 is None: #comprobacion de que si algun jugador se quedo sin cartas en su mazo
       if len(self.jugador_1.mazo) > len(self.jugador_2.mazo):
         self.ganador = "Jugador 1"
       elif len(self.jugador_2.mazo) > len(self.jugador_1.mazo):
         self.ganador= "Jugador 2"
 
+    #lineas que imprime en la consola segun el estado actual del juego
     print("\n-------------------------------------")
     print(f"Turno: {self.turno}")
     print("Jugador 1:")
@@ -116,6 +170,7 @@ class JuegoDeGuerra:
     self.turno +=1
     botin=Mazo()
     
+    #comprobacion de que si algun jugador no tiene las suficientes cartas para jugar la guerra
     if len(self.jugador_1.mazo) < 4 or len(self.jugador_2.mazo) < 4:
       if len(self.jugador_1.mazo) < 4:
         self.ganador = "Jugador 2"
@@ -126,14 +181,14 @@ class JuegoDeGuerra:
       else:
         self.turno = self.max_turnos + 1
 
-    carta_jugador_1 = self.jugador_1.sacar_arriba(1)
+    carta_jugador_1 = self.jugador_1.sacar_arriba(1) #extrae las cartas que dieron como resultado guerra
     carta_jugador_2 = self.jugador_2.sacar_arriba(2)
-    botin.poner_abajo(carta_jugador_1)
+    botin.poner_abajo(carta_jugador_1) #las guarda en un botin
     botin.poner_abajo(carta_jugador_2)
 
-    hay_guerra= True
-    while hay_guerra:
-      for h in range(3):
+    hay_guerra= True 
+    while hay_guerra: #para cada vez que este en True, sigue jugandose una guerra
+      for h in range(3): #saca 3 cartas de cada jugador y las guarda en botin
         carta1 = self.jugador_1.sacar_arriba(1)
         carta2 = self.jugador_2.sacar_arriba(2)
 
@@ -151,17 +206,15 @@ class JuegoDeGuerra:
           hay_guerra= False
           self.turno = self.max_turnos + 1
 
-      carta_jugador_guerra_1 = self.jugador_1.sacar_arriba(1)
+      carta_jugador_guerra_1 = self.jugador_1.sacar_arriba(1) #las nuevas cartas que se comparan
       carta_jugador_guerra_2 = self.jugador_2.sacar_arriba(2)
+      botin.poner_abajo(carta_jugador_guerra_1)
+      botin.poner_abajo(carta_jugador_guerra_2)
 
       print(f"Turno: {self.turno}")
       print("Jugador 1:")
       self.imprimir_mazo(self.jugador_1)
-      print()
-      print(f"         {carta_jugador_1} {carta_jugador_2}", end=" ")
-      print("-X " * 6, end="")
-      print(f"{carta_jugador_guerra_1} {carta_jugador_guerra_2}")
-      print("\n")
+      self.imprimir_botin_guerra(botin) #imprime el botin en juego (las cartas que se llevara el ganador)
       print("Jugador 2:")
       self.imprimir_mazo(self.jugador_2)
 
@@ -170,25 +223,20 @@ class JuegoDeGuerra:
 
       if valor_carta_guerra_1 > valor_carta_guerra_2:
         for x in botin:
-          self.jugador_1.poner_abajo(x)
-        self.jugador_1.poner_abajo(carta_jugador_guerra_1)
-        self.jugador_1.poner_abajo(carta_jugador_guerra_2)
-        hay_guerra= False
+          self.jugador_1.poner_abajo(x) #guarda todo el botin en el ganador en el orden correspondiete
+        hay_guerra= False #cambia el estado para que salga del bucle
         print("\nJugador 1 gana la ronda")
 
       elif valor_carta_guerra_1 < valor_carta_guerra_2:
         for x in botin:
           self.jugador_2.poner_abajo(x)
-        self.jugador_2.poner_abajo(carta_jugador_guerra_1)
-        self.jugador_2.poner_abajo(carta_jugador_guerra_2)
+
         hay_guerra= False
         print("\nJugador 2 gana la ronda")
 
       else:
         print("\n-------------------------------------")
-        print(" " * 25, "**** Nuevamente Guerra!! ****")
-        botin.poner_abajo(carta_jugador_guerra_1)
-        botin.poner_abajo(carta_jugador_guerra_2)
+        print(" " * 25, "**** Nuevamente Guerra!! ****") #mensaje de otra guerra seguida
         self.turno +=1
 
   def game_play(self):
@@ -199,7 +247,7 @@ class JuegoDeGuerra:
     while self.turno <= self.max_turnos:
       self.jugar_ronda()
 
-    if self.ganador:
+    if self.ganador: #comprobacion de que si hay un ganador o si hay un empate
       print(f"\n                               ***** {self.ganador} gana la partida *****")
     elif self.turno > self.max_turnos:
       print("Limite alcanzado")
